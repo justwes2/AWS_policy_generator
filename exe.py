86c37d1ec31eab712e9c7666362e22cfc3dc3384
Wes <wes.coffay@gmail.com>
2018-03-02 16:25:10 -0500

    poc achieved

--- exe.py
+++ exe.py
@@ -1,2 +1,9 @@
-policy = open("EC2FullAccess.json", "r")
-template = policy.read()
+template = open("EC2FullAccess.json", "r").read()
+
+apps_raw = open("AWS_policy_mgmt/appList.txt", "r").readlines()
+apps = [app.rstrip() for app in apps_raw]
+
+for app in apps:
+    fh = open("EC2FullAccess_{}.json".format(app), "w")
+    fh.write(template.format(app))
+    fh.close()e.format(app)
